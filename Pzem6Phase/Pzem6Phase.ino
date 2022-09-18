239f3c794060d6ce1ef45defd176b85a63a0de4e
canusorn <anusorn1998@gmail.com>
2022-09-18 19:12:51 +0700

    start

--- Pzem6Phase/Pzem6Phase.ino
+++ Pzem6Phase/Pzem6Phase.ino
@@ -1,6 +1,7 @@
 
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <HTTPClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <HTTPUpdateServer.h>
@@ -38,6 +39,10 @@ void setup() {
     Serial.println("WiFi failed, retrying.");
   }
 
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
   MDNS.begin(host);
   if (MDNS.begin(host)) {
     Serial.println("mDNS responder started");
@@ -57,7 +62,7 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= 2000) {
     previousMillis = currentMillis;
-    displayValue();
+    readValue();
     Serial.println();
   }
 
@@ -65,7 +70,7 @@ void loop() {
 }
 
 
-void displayValue()
+void readValue()
 {
 
   PZEM004Tv30 pzems[] = {PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x01), PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x02), PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x03), PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x04), PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x05), PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x06)}; // array of pzem 3 phase
@@ -126,4 +131,59 @@ void displayValue()
     Serial.println();
   }
 
+  postData(voltage, current, power, energy, frequency, pf);
+}
+
+void postData(float v[6], float a[6], float p[6], float e[6], float f[6], float pf[6]) {
+
+  if ((WiFi.status() == WL_CONNECTED)) {
+
+//    for (uint8_t i = 0; i < 6; i++)
+//    {
+//      if (var_index_3p[i])
+//      { // validate
+//        if (v >= 60 && v <= 260 && !isnan(v))
+//          _json_update += ",\"v" + String(i + 1) + "\":" + String(v, 1);
+//        if (i >= 0 && i <= 100 && !isnan(i))
+//          _json_update += ",\"i" + String(i + 1) + "\":" + String(a, 3);
+//        if (p >= 0 && p <= 24000 && !isnan(p))
+//          _json_update += ",\"p" + String(i + 1) + "\":" + String(p, 1);
+//        if (e >= 0 && e <= 10000 && !isnan(e))
+//          _json_update += ",\"e" + String(i + 1) + "\":" + String(e, 3);
+//        if (f >= 40 && f <= 70 && !isnan(f))
+//          _json_update += ",\"f" + String(i + 1) + "\":" + String(f, 1);
+//        if (pf >= 0 && pf <= 1 && !isnan(pf))
+//          _json_update += ",\"pf" + String(i + 1) + "\":" + String(pf, 2);
+//      }
+//    }
+
+
+
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    // configure traged server and url
+    //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+    http.begin("http://example.com/index.html"); //HTTP
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
 }
