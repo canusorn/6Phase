d255050e795c21d50edc27a295681f95f26df0fc
canusorn <anusorn1998@gmail.com>
2022-09-20 22:21:41 +0700

    add login page

--- index.php
+++ index.php
@@ -1,2 +1,167 @@
 <?php
-echo "hello";
+
+require 'includes/init.php';
+
+if (isset($_SESSION['is_login'])) {
+    header("Location: http://" . $_SERVER['HTTP_HOST'] . '/dashboard');
+    exit;
+} else if (isset($_COOKIE["is_login"])) {
+    session_regenerate_id(true);
+
+    $_SESSION['is_login'] = true;
+
+    setcookie("is_login", true, time() + 3600 * 24 * 7); // Expire 7 day
+
+    header("Location: http://" . $_SERVER['HTTP_HOST'] . '/dashboard');
+    exit;
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if ($_POST['user'] == USER && $_POST['pass'] == PASS) {
+        session_regenerate_id(true);
+
+        $_SESSION['is_login'] = true;
+
+        setcookie("is_login", true, time() + 3600 * 24 * 7); // Expire 7 day
+
+        header("Location: http://" . $_SERVER['HTTP_HOST'] . '/dashboard');
+        exit;
+    } else {
+        $error = "Wrong username and password";
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>6 Phase Login</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #f5f5f5;
+        }
+
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+        }
+
+        .form-signin .form-floating:focus-within {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
+
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
+
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
+
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
+    </style>
+
+</head>
+
+<body class="text-center">
+
+    <main class="form-signin w-100 m-auto">
+        <form method="post">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <?php if (isset($error)) {
+                echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+            }
+            ?>
+
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInput" name="user" placeholder="username">
+                <label for="floatingInput">Username</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" name="pass" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    </main>
+
+
+</body>
+
+</html>
